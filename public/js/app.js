0aec92a4dbdf734a1b162135c8bd710decf06927
TheBlankness <iddinishak@gmail.com>
2019-11-27 13:55:44 +0800

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -4,17 +4,6 @@ fetch("http://puzzle.mead.io/puzzle").then(response => {
   });
 });
 
-fetch("http://localhost:3000/weather?address=boston").then(response => {
-  response.json().then(data => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.location);
-      console.log(data.forecast);
-    }
-  });
-});
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
@@ -25,7 +14,7 @@ weatherForm.addEventListener("submit", e => {
   messageOne.textContent = "Loading..";
   const location = search.value;
 
-  fetch("http://localhost:3000/weather?address=" + location).then(response => {
+  fetch("/weather?address=" + location).then(response => {
     response.json().then(data => {
       if (data.error) {
         messageOne.textContent = data.error;
